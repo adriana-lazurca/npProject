0002a7e426c58a196e77eb1a044995f15d18b9f8
Adriana Lazurca <adrianar.lazurca@gmail.com>
2021-06-24 14:58:01 +0300

    add id

--- src/constants/Rooms.js
+++ src/constants/Rooms.js
@@ -1,20 +1,24 @@
 export const ROOMS = [
     {
+        id:1,
         name: 'Special Room',
         //category: 'family',
         //price: 100, 
     },
     {
+        id:2,
         name: 'Second Room',
         category: 'family',
         price: 150, 
     },
     {
+        id:3,
         name: 'Luxury Room',
         category: 'family',
         price: 300, 
     },
     {
+        id:4,
         name: 'Small Room',
         category: 'family',
         price: 600, 
